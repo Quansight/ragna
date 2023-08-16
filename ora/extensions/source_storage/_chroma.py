47f4033ac1e8b3f2afad1fc8d30293037d9301de
Philip Meier <github.pmeier@posteo.de>
2023-08-17 00:37:17 +0200

    improve chroma

--- ora/extensions/source_storage/_chroma.py
+++ ora/extensions/source_storage/_chroma.py
@@ -1,5 +1,6 @@
-import functools
-import hashlib
+import enum
+
+from typing import Any, TYPE_CHECKING
 
 from ora.extensions import (
     Document,
@@ -9,7 +10,17 @@ from ora.extensions import (
     Source,
     SourceStorage,
 )
-from ora.utils import chunk_pages, page_numbers_to_str
+from ora.utils import chunk_pages, compute_id, page_numbers_to_str
+
+if TYPE_CHECKING:
+    import chromadb
+    import chromadb.utils.embedding_functions
+    import tiktoken
+
+
+class ChromaResultType(enum.Enum):
+    GET = enum.auto()
+    QUERY = enum.auto()
 
 
 class ChromaSourceStorage(SourceStorage):
@@ -19,28 +30,62 @@ class ChromaSourceStorage(SourceStorage):
 
     @classmethod
     def requirements(cls) -> list[Requirement]:
-        return [PackageRequirement("chromadb >=0.4")]
+        return [PackageRequirement("chromadb >=0.4"), PackageRequirement("tiktoken")]
 
-    def __init__(self, app_config, embedding_function=None, tokenizer=None):
+    # This needs to be a class variable to enable sharing it between multiple instances.
+    # See https://docs.trychroma.com/usage-guide#initiating-a-persistent-chroma-client
+    # for details
+    _client: "chromadb.Client | None" = None
+
+    def __init__(
+        self,
+        app_config,
+        embedding_function: "chromadb.utils.embedding_functions.EmbeddingFunction | None" = None,
+        tokenizer: "tiktoken.Encoding | None" = None,
+    ):
         super().__init__(app_config)
         import chromadb
         import chromadb.utils.embedding_functions
+        import tiktoken
 
-        # FIXME: pull cache path from app_config
-        self._client = chromadb.Client(
-            chromadb.config.Settings(anonymized_telemetry=False)
-        )
+        if self._client is None:
+            self._client = chromadb.Client(
+                chromadb.config.Settings(
+                    is_persistent=True,
+                    persist_directory=str(app_config.cache_root / "chroma"),
+                    anonymized_telemetry=False,
+                )
+            )
 
-        # FIXME use a free one here
         self._embedding_function = (
             embedding_function
-            or chromadb.utils.embedding_functions.OpenAIEmbeddingFunction()
+            or chromadb.utils.embedding_functions.DefaultEmbeddingFunction()
         )
-        self._tokenizer = tokenizer
+        self._tokenizer = tokenizer or tiktoken.get_encoding("cl100k_base")
 
-    @functools.lru_cache(maxsize=1024)
     def _collection_name(self, user_name: str) -> str:
-        return hashlib.md5(user_name.encode()).hexdigest()
+        return compute_id(user_name)
+
+    def _parse_result(
+        self, result: dict[str, list | None], *, result_type: ChromaResultType
+    ) -> list[dict[str, Any]]:
+        if result_type is ChromaResultType.QUERY:
+            result = {
+                key: value if value is None else value[0]
+                for key, value in result.items()
+            }
+
+        # ids are always returned so we can use that as starting point
+        num_results = len(result["ids"])
+        result = {
+            key: [None] * num_results if value is None else value
+            for key, value in result.items()
+        }
+        # dict of lists -> list of dicts
+        return [
+            {key.rstrip("s"): value[idx] for key, value in result.items()}
+            for idx in range(num_results)
+        ]
 
     def _extract_chunk_params(self, chat_config) -> tuple[int, int]:
         chunk_size = chat_config.extra.get("chunk_size", 500)
@@ -49,7 +94,7 @@ class ChromaSourceStorage(SourceStorage):
 
     def store(self, documents: list[Document], chat_config) -> None:
         collection = self._client.get_or_create_collection(
-            self._collection_name(self.app_config.user_name)
+            self._collection_name(self.app_config.user)
         )
 
         chunk_size, chunk_overlap = self._extract_chunk_params(chat_config)
@@ -58,9 +103,18 @@ class ChromaSourceStorage(SourceStorage):
         documents_to_store: list[str] = []
         metadatas_to_store: list[dict[str, str]] = []
         for document in documents:
-            # check if we already have documents with id and chunking in the collection
-            # if so, continue
-            if False:
+            result = collection.get(
+                where={
+                    "$and": [
+                        {"document_id": {"$eq": document.id}},
+                        {"chunk_size": {"$eq": chunk_size}},
+                        {"chunk_overlap": {"$eq": chunk_overlap}},
+                    ]
+                },
+                limit=1,
+                include=["metadatas"],
+            )
+            if self._parse_result(result, result_type=ChromaResultType.GET):
                 continue
 
             pages = document.extract_pages()
@@ -74,15 +128,15 @@ class ChromaSourceStorage(SourceStorage):
                 )
             ):
                 ids_to_store.append(
-                    hashlib.md5(
-                        f"{document.name}--{chunk_size}--{chunk_overlap}--{idx}".encode()
-                    ).hexdigest()
+                    compute_id(document.id, chunk_size, chunk_overlap, idx)
                 )
                 documents_to_store.append(chunk.text)
                 metadatas_to_store.append(
                     {
-                        "doc_name": document.name,
-                        "doc_id": document.id,
+                        "document_name": document.name,
+                        "document_id": document.id,
+                        "chunk_size": chunk_size,
+                        "chunk_overlap": chunk_overlap,
                         "num_tokens": chunk.num_tokens,
                         "page_numbers": page_numbers_to_str(chunk.page_numbers),
                     }
@@ -97,11 +151,21 @@ class ChromaSourceStorage(SourceStorage):
 
     def retrieve(self, prompt: str, *, num_tokens: int, chat_config) -> list[Source]:
         collection = self._client.get_collection(
-            self._collection_name(self.app_config.user_name)
+            self._collection_name(self.app_config.user)
         )
 
-        chunk_size, _ = self._extract_chunk_params(chat_config)
-        results = collection.query(
+        document_id_filters = [
+            {"document_id": {"$eq": metadata.id}}
+            for metadata in chat_config.document_metadatas
+        ]
+        if len(document_id_filters) == 1:
+            document_id_filter = document_id_filters[0]
+        else:
+            document_id_filter = {"$or": document_id_filters}
+
+        chunk_size, chunk_overlap = self._extract_chunk_params(chat_config)
+
+        result = collection.query(
             query_texts=prompt,
             n_results=min(
                 # We cannot retrieve source by a maximum number of tokens. Thus, we
@@ -111,21 +175,26 @@ class ChromaSourceStorage(SourceStorage):
                 int(num_tokens * 2 / chunk_size),
                 collection.count(),
             ),
-            # filter based on doc id from chat config here
+            where={
+                "$and": [
+                    document_id_filter,
+                    {"chunk_size": {"$eq": chunk_size}},
+                    {"chunk_overlap": {"$eq": chunk_overlap}},
+                ]
+            },
         )
-        # Chroma results come as dict of lists, but we need list of dicts here
-
-        # sort them by distance ascending. I think they are already sorted, but let's
-        # make sure
+        results = self._parse_result(result, result_type=ChromaResultType.QUERY)
+        # That should be the default, but let's make extra sure here
+        results = sorted(results, key=lambda r: r["distance"])
 
         # FIXME: implement culling here based on distance
+        # FIXME: Limit number of tokens
 
-        # Limit number of tokens
         return [
             Source(
-                name=result["metadatas"]["doc_name"],
-                location=result["metadatas"]["page_numbers"],
-                text=result["documents"],
+                name=result["metadata"]["document_name"],
+                location=result["metadata"]["page_numbers"],
+                text=result["document"],
             )
             for result in results
         ]
--- ora/utils/_chunk.py
+++ ora/utils/_chunk.py
@@ -7,11 +7,13 @@ from ora._backend import Page
 @dataclasses.dataclass
 class Chunk:
     text: str
-    page_numbers: list[int]
+    page_numbers: list[int] | None
     num_tokens: int
 
 
 def chunk_pages(
     pages: Sequence[Page], *, chunk_size: int, chunk_overlap: int, tokenizer: str
 ) -> list[Chunk]:
-    pass
+    # FIXME
+    page = pages[0]
+    return [Chunk(page.text, page.number, chunk_size)]
