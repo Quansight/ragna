53513bf466b565ba5695f3a2658b683997a7c831
Philip Meier <github.pmeier@posteo.de>
2023-09-14 22:07:29 +0200

    add local installation instructions as well as minimal example (#35)

--- ragna/llm/_demo.py
+++ ragna/llm/_demo.py
@@ -1,4 +1,5 @@
 import sys
+import textwrap
 
 from ragna.core import Llm, Source
 
@@ -13,7 +14,26 @@ class RagnaDemoLlm(Llm):
         return sys.maxsize
 
     def complete(self, prompt: str, sources: list[Source]) -> str:
-        return (
-            "I'm just pretending to be an LLM, "
-            "so I can't actually help with your prompt."
-        )
+        sources_display = [
+            (
+                f"- {source.document_name}, page(s) {source.page_numbers}: "
+                f"{textwrap.shorten(source.text, width=100)}"
+            )
+            for source in sources[:3]
+        ]
+        if len(sources) > 3:
+            sources_display.append("[...]")
+
+        return textwrap.dedent(
+            """
+        I'm {name} and I just pretend to be an LLM. I can't actually help with your prompt:
+        
+        > {prompt}
+        
+        I was given the following sources:
+        
+        {sources}
+        """.format(
+                name=str(self), prompt=prompt, sources="\n".join(sources_display)
+            )
+        ).strip()
