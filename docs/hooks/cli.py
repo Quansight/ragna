3d7e848c6a1085fa802217cf812e75fe59d86e4c
Philip Meier <github.pmeier@posteo.de>
2023-10-12 20:48:50 +0200

    automatically build CLI docs

--- docs/hooks/cli.py
+++ docs/hooks/cli.py
@@ -2,11 +2,16 @@ import contextlib
 import io
 from pathlib import Path
 
+from mkdocs.config.defaults import MkDocsConfig
+
 from ragna._cli import app
 
+HERE = Path(__file__).parent
+DOCS_ROOT = HERE.parent
 
-def main():
-    with open(Path(__file__).parent / "cli.md", "w") as file:
+
+def on_pre_build(config: MkDocsConfig) -> None:
+    with open(DOCS_ROOT / "references" / "cli.md", "w") as file:
         file.write(f"# CLI reference\n\n{get_doc(None)}")
         for command in app.registered_commands:
             file.write(get_doc(command.name or command.callback.__name__))
@@ -28,7 +33,3 @@ def get_help(command):
     lines = stdout.getvalue().strip().splitlines()
     lines[0] = lines[0].replace(Path(__file__).name, "ragna")
     return "\n".join(line.strip() for line in lines)
-
-
-if __name__ == "__main__":
-    main()
