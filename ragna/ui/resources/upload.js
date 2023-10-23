21f57c068ce49c209b3809f7f7802d81369b89fb
Philip Meier <github.pmeier@posteo.de>
2023-10-23 22:36:12 +0200

    extend prettier autoformatter to JS and CSS

--- ragna/ui/resources/upload.js
+++ ragna/ui/resources/upload.js
@@ -1,58 +1,45 @@
-
-
 function upload(files, token, informationsEndpoint, final_callback) {
+  var uploaded_documents = [];
 
-    var uploaded_documents = [] 
-
-    Array.from(files).map(file => {
-
-        getUploadInformations(file, token, informationsEndpoint).then(
-
-                function(uploadInformations) {
-
-                        uploadFile(uploadInformations.url, uploadInformations.data, file).then(
-                            function(response) {
-                                uploaded_documents.push(response)
-                                if (uploaded_documents.length == files.length) {
-                                    final_callback(uploaded_documents);
-                                }
-                            }
-
-
-                        )
-                }
+  Array.from(files).map((file) => {
+    getUploadInformations(file, token, informationsEndpoint).then(
+      function (uploadInformations) {
+        uploadFile(uploadInformations.url, uploadInformations.data, file).then(
+          function (response) {
+            uploaded_documents.push(response);
+            if (uploaded_documents.length == files.length) {
+              final_callback(uploaded_documents);
+            }
+          },
         );
-        
-    })
-    
-    
+      },
+    );
+  });
 }
 
 async function getUploadInformations(file, token, informationsEndpoint) {
-        const response = await fetch(
-            `${informationsEndpoint}?name=${file.name}`,
-            {"headers": {"Authorization": `Bearer ${token}`}},
-        );
-        const uploadInformations = await response.json();
+  const response = await fetch(`${informationsEndpoint}?name=${file.name}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  const uploadInformations = await response.json();
 
-        return uploadInformations
+  return uploadInformations;
 }
 
 async function uploadFile(url, data, file) {
-    var body = new FormData()
+  var body = new FormData();
 
-    for (const [key, value] of Object.entries(data)) {
-      body.append(key, value);
-    }
-    body.append('file', file );
+  for (const [key, value] of Object.entries(data)) {
+    body.append(key, value);
+  }
+  body.append("file", file);
 
-    const payload = {
-        method: 'POST',
-        body: body
-    }
+  const payload = {
+    method: "POST",
+    body: body,
+  };
 
-    const response = fetch(url, payload).then((response) => response.json())
-    
-    return response
-    
+  const response = fetch(url, payload).then((response) => response.json());
+
+  return response;
 }
