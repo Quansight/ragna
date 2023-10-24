1611044c4384ee0acebc40432bcab2550431fcce
Pierre-Olivier Simonard <pierre.olivier.simonard@gmail.com>
2023-10-24 10:41:57 +0200

    improved upload by @pmeier
    following up this discussion : https://github.com/Quansight/ragna/pull/61#discussion_r1369752049

--- ragna/ui/resources/upload.js
+++ ragna/ui/resources/upload.js
@@ -1,44 +1,27 @@
 function upload(files, token, informationsEndpoint, final_callback) {
-  var uploaded_documents = [];
-
-  Array.from(files).map((file) => {
-    getUploadInformations(file, token, informationsEndpoint).then(
-      function (uploadInformations) {
-        uploadFile(uploadInformations.url, uploadInformations.data, file).then(
-          () => undefined,
-        );
-        uploaded_documents.push(uploadInformations.document);
-        if (uploaded_documents.length == files.length) {
-          final_callback(uploaded_documents);
-        }
-      },
-    );
-  });
+  Promise.all(
+    Array.from(files).map((file) => {
+      return uploadFile(file, token, informationsEndpoint);
+    }),
+  ).then(final_callback);
 }
 
-async function getUploadInformations(file, token, informationsEndpoint) {
-  const response = await fetch(`${informationsEndpoint}?name=${file.name}`, {
+async function uploadFile(file, token, informationEndpoint) {
+  const response = await fetch(`${informationEndpoint}?name=${file.name}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
-  const uploadInformations = await response.json();
+  const documentInfo = await response.json();
 
-  return uploadInformations;
-}
-
-async function uploadFile(url, data, file) {
   var body = new FormData();
-
-  for (const [key, value] of Object.entries(data)) {
+  for (const [key, value] of Object.entries(documentInfo.data)) {
     body.append(key, value);
   }
   body.append("file", file);
 
-  const payload = {
+  await fetch(documentInfo.url, {
     method: "POST",
     body: body,
-  };
+  });
 
-  const response = fetch(url, payload).then((response) => response.json());
-
-  return response;
+  return documentInfo.document;
 }
