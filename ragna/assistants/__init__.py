05191d99e7ce1e9f57990231d4a3aa9b9d8805c3
Philip Meier <github.pmeier@posteo.de>
2023-10-26 10:36:37 +0200

    cleanup usort (#111)

--- ragna/assistants/__init__.py
+++ ragna/assistants/__init__.py
@@ -1,7 +1,9 @@
-from ragna._utils import fix_module  # usort: skip
-
 from ._demo import RagnaDemoAssistant
 from ._openai import Gpt4, Gpt35Turbo16k
+
+# isort: split
+
+from ragna._utils import fix_module
 
 fix_module(globals())
 del fix_module
