9f8830a0d665baaca603c17f663d3df59e8df1ff
Philip Meier <github.pmeier@posteo.de>
2023-10-31 23:08:14 +0100

    dont create DB session as dependency

--- ragna/_api/core.py
+++ ragna/_api/core.py
@@ -1,3 +1,4 @@
+import contextlib
 import uuid
 from typing import Annotated, Any, Iterator, Type, cast
 
@@ -90,52 +91,50 @@ def app(config: Config) -> FastAPI:
         database_url = "sqlite://"
     make_session = database.get_sessionmaker(database_url)
 
+    @contextlib.contextmanager
     def get_session() -> Iterator[database.Session]:
-        session = make_session()
-        try:
+        with make_session() as session:
             yield session
-        finally:
-            session.close()
-
-    SessionDependency = Annotated[database.Session, Depends(get_session)]
 
     @app.get("/document")
     async def get_document_upload_info(
-        session: SessionDependency,
         user: UserDependency,
         name: str,
     ) -> schemas.DocumentUploadInfo:
-        document = schemas.Document(name=name)
-        url, data, metadata = await config.core.document.get_upload_info(
-            config=config, user=user, id=document.id, name=document.name
-        )
-        database.add_document(session, user=user, document=document, metadata=metadata)
-        return schemas.DocumentUploadInfo(url=url, data=data, document=document)
+        with get_session() as session:
+            document = schemas.Document(name=name)
+            url, data, metadata = await config.core.document.get_upload_info(
+                config=config, user=user, id=document.id, name=document.name
+            )
+            database.add_document(
+                session, user=user, document=document, metadata=metadata
+            )
+            return schemas.DocumentUploadInfo(url=url, data=data, document=document)
 
     @app.post("/document")
     async def upload_document(
-        session: SessionDependency, token: Annotated[str, Form()], file: UploadFile
+        token: Annotated[str, Form()], file: UploadFile
     ) -> schemas.Document:
         if not issubclass(rag.config.core.document, ragna.core.LocalDocument):
             raise HTTPException(
                 status_code=400,
                 detail="Ragna configuration does not support local upload",
             )
+        with get_session() as session:
+            user, id = ragna.core.LocalDocument.decode_upload_token(
+                token, secret=rag.config.api.upload_token_secret
+            )
+            document, metadata = database.get_document(session, user=user, id=id)
 
-        user, id = ragna.core.LocalDocument.decode_upload_token(
-            token, secret=rag.config.api.upload_token_secret
-        )
-        document, metadata = database.get_document(session, user=user, id=id)
+            core_document = ragna.core.LocalDocument(
+                id=document.id, name=document.name, metadata=metadata
+            )
+            core_document.path.parent.mkdir(parents=True, exist_ok=True)
+            async with aiofiles.open(core_document.path, "wb") as document_file:
+                while content := await file.read(1024):
+                    await document_file.write(content)
 
-        core_document = ragna.core.LocalDocument(
-            id=document.id, name=document.name, metadata=metadata
-        )
-        core_document.path.parent.mkdir(parents=True, exist_ok=True)
-        async with aiofiles.open(core_document.path, "wb") as document_file:
-            while content := await file.read(1024):
-                await document_file.write(content)
-
-        return document
+            return document
 
     def schema_to_core_chat(
         session: database.Session, *, user: str, chat: schemas.Chat
@@ -171,68 +170,67 @@ def app(config: Config) -> FastAPI:
 
     @app.post("/chats")
     async def create_chat(
-        session: SessionDependency,
         user: UserDependency,
         chat_metadata: schemas.ChatMetadata,
     ) -> schemas.Chat:
-        chat = schemas.Chat(metadata=chat_metadata)
+        with get_session() as session:
+            chat = schemas.Chat(metadata=chat_metadata)
 
-        # Although we don't need the actual ragna.core.Chat object here,
-        # we use it to validate the documents and metadata.
-        schema_to_core_chat(session, user=user, chat=chat)
+            # Although we don't need the actual ragna.core.Chat object here,
+            # we use it to validate the documents and metadata.
+            schema_to_core_chat(session, user=user, chat=chat)
 
-        database.add_chat(session, user=user, chat=chat)
-        return chat
+            database.add_chat(session, user=user, chat=chat)
+            return chat
 
     @app.get("/chats")
-    async def get_chats(
-        session: SessionDependency, user: UserDependency
-    ) -> list[schemas.Chat]:
-        return database.get_chats(session, user=user)
+    async def get_chats(user: UserDependency) -> list[schemas.Chat]:
+        with get_session() as session:
+            return database.get_chats(session, user=user)
 
     @app.get("/chats/{id}")
-    async def get_chat(
-        session: SessionDependency, user: UserDependency, id: uuid.UUID
-    ) -> schemas.Chat:
-        return database.get_chat(session, user=user, id=id)
+    async def get_chat(user: UserDependency, id: uuid.UUID) -> schemas.Chat:
+        with get_session() as session:
+            return database.get_chat(session, user=user, id=id)
 
     @app.post("/chats/{id}/prepare")
     async def prepare_chat(
-        session: SessionDependency, user: UserDependency, id: uuid.UUID
+        user: UserDependency, id: uuid.UUID
     ) -> schemas.MessageOutput:
-        chat = database.get_chat(session, user=user, id=id)
+        with get_session() as session:
+            chat = database.get_chat(session, user=user, id=id)
 
-        core_chat = schema_to_core_chat(session, user=user, chat=chat)
-        welcome = schemas.Message.from_core(await core_chat.prepare())
-        chat.prepared = True
-        chat.messages.append(welcome)
+            core_chat = schema_to_core_chat(session, user=user, chat=chat)
+            welcome = schemas.Message.from_core(await core_chat.prepare())
+            chat.prepared = True
+            chat.messages.append(welcome)
 
-        database.update_chat(session, user=user, chat=chat)
+            database.update_chat(session, user=user, chat=chat)
 
-        return schemas.MessageOutput(message=welcome, chat=chat)
+            return schemas.MessageOutput(message=welcome, chat=chat)
 
     @app.post("/chats/{id}/answer")
     async def answer(
-        session: SessionDependency, user: UserDependency, id: uuid.UUID, prompt: str
+        user: UserDependency, id: uuid.UUID, prompt: str
     ) -> schemas.MessageOutput:
-        chat = database.get_chat(session, user=user, id=id)
-        chat.messages.append(
-            schemas.Message(content=prompt, role=ragna.core.MessageRole.USER)
-        )
+        with get_session() as session:
+            chat = database.get_chat(session, user=user, id=id)
+            chat.messages.append(
+                schemas.Message(content=prompt, role=ragna.core.MessageRole.USER)
+            )
 
-        core_chat = schema_to_core_chat(session, user=user, chat=chat)
+            core_chat = schema_to_core_chat(session, user=user, chat=chat)
 
-        answer = schemas.Message.from_core(await core_chat.answer(prompt))
-        chat.messages.append(answer)
+            answer = schemas.Message.from_core(await core_chat.answer(prompt))
+            chat.messages.append(answer)
 
-        database.update_chat(session, user=user, chat=chat)
+            database.update_chat(session, user=user, chat=chat)
 
-        return schemas.MessageOutput(message=answer, chat=chat)
+            return schemas.MessageOutput(message=answer, chat=chat)
 
     @app.delete("/chats/{id}")
-    async def delete_chat(
-        session: SessionDependency, user: UserDependency, id: uuid.UUID
-    ) -> None:
-        database.delete_chat(session, user=user, id=id)
+    async def delete_chat(user: UserDependency, id: uuid.UUID) -> None:
+        with get_session() as session:
+            database.delete_chat(session, user=user, id=id)
 
     return app
