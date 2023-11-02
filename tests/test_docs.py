40157313f5847a687127a3c3d5881b738f875b83
Philip Meier <github.pmeier@posteo.de>
2023-11-03 00:15:29 +0100

    mock writing to files

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -4,13 +4,15 @@ import pytest
 from mktestdocs import check_md_file
 
 
-@pytest.mark.parametrize("fpath", pathlib.Path("docs").glob("**/*.md"), ids=str)
-def test_files_good(fpath):
+@pytest.mark.parametrize("path", pathlib.Path("docs").glob("**/*.md"), ids=str)
+def test_files_good(mocker, path):
+    mocker.patch("builtins.open", mocker.mock_open())
+
     # FIXME: The REST API tutorial uses await outside sync functions
     # this would work in a Jupyter Notebook, but not in a Python script
     # We'll also need to have the API running to test this properly.
-    if fpath == pathlib.Path("docs/tutorials/rest-api.md"):
+    if path == pathlib.Path("docs/tutorials/rest-api.md"):
         with pytest.raises(SyntaxError, match="'await' outside function"):
-            check_md_file(fpath=fpath, memory=True)
+            check_md_file(path, memory=True)
     else:
-        check_md_file(fpath=fpath, memory=True)
+        check_md_file(path, memory=True)
