0d8927b0acb08983d321993f864d682f8fe1326a
peachkeel <150266856+peachkeel@users.noreply.github.com>
2023-12-11 02:19:03 +0000

    cap number of concurrent file uploads so Chrome/Chromium is not overwhelmed causing the Ragna UI to hang (#176)

--- ragna/deploy/_ui/resources/upload.js
+++ ragna/deploy/_ui/resources/upload.js
@@ -1,9 +1,35 @@
 function upload(files, token, informationsEndpoint, final_callback) {
-  Promise.all(
-    Array.from(files).map((file) => {
-      return uploadFile(file, token, informationsEndpoint);
-    }),
-  ).then(final_callback);
+  const batchSize = 500; // Maximum number of concurrent uploads
+  let currentIndex = 0; // Tracks the index of the current file to be uploaded
+  let successfullyUploaded = []; // Array to hold the results
+
+  // Function to upload a single batch of files
+  function uploadBatch() {
+    // Get the next batch of files based on the current index and batch size
+    const batch = Array.from(files).slice(currentIndex, currentIndex + batchSize);
+    currentIndex += batchSize;
+    // Map over the batch to create an array of upload promises
+    return Promise.all(
+      batch.map(file => uploadFile(file, token, informationsEndpoint))
+    ).then(results => {
+      // Concatenate the results of the current batch to the main array
+      successfullyUploaded = successfullyUploaded.concat(results);
+      // If there are more files to upload, recursively call uploadBatch again
+      if (currentIndex < files.length) {
+        return uploadBatch();
+      } else {
+        // If all files are uploaded, invoke the final callback with the results
+        final_callback(successfullyUploaded);
+      }
+    });
+  }
+
+  // Start uploading the first batch
+  uploadBatch().catch(error => {
+    console.error("An error occurred during the upload process", error);
+    // You might want to call your final callback with an error or partial results
+    final_callback(successfullyUploaded);
+  });
 }
 
 async function uploadFile(file, token, informationEndpoint) {
