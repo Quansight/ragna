3a45e771fff8f0e2af591325705b7fd90dfe606b
Philip Meier <github.pmeier@posteo.de>
2024-01-09 15:43:00 +0100

    dirty

--- ragna/deploy/_api/ui.py
+++ ragna/deploy/_api/ui.py
@@ -1,10 +1,13 @@
 import asyncio
+import inspect
+import random
+import uuid
 from pathlib import Path
-from typing import Annotated, Any
+from typing import Annotated, Any, get_type_hints
 
 import fast_html as f7l
 import sse_starlette
-from fastapi import APIRouter, Form, Request
+from fastapi import APIRouter, BackgroundTasks, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -16,12 +19,67 @@ class FastHtmlResponse(HTMLResponse):
 
 router = APIRouter(default_response_class=FastHtmlResponse)
 
-templates = Jinja2Templates(directory=Path(__file__).parent / "templates")
+
+import functools
+
+
+class Templates(Jinja2Templates):
+    def __call__(self, template_name: str):
+        def decorator(endpoint):
+            # The template response requires the request as input. If it not is not
+            # required by the endpoint, we add it to the signature for it to be supplied
+            # automatically by FastAPI.
+            signature = inspect.signature(endpoint)
+            parameters = list(signature.parameters.values())
+            for name, annotation in get_type_hints(endpoint).items():
+                if isinstance(annotation, type) and issubclass(annotation, Request):
+                    request_name, forward_request = name, True
+                    break
+            else:
+                request_name, forward_request = "__request__", False
+                parameters.append(
+                    inspect.Parameter(
+                        name=request_name,
+                        kind=inspect.Parameter.POSITIONAL_OR_KEYWORD,
+                        annotation=Request,
+                    )
+                )
+            signature = signature.replace(parameters=parameters)
+
+            @functools.wraps(endpoint)
+            async def wrapper(**values):
+                request = values[request_name]
+                if not forward_request:
+                    del values[request_name]
+
+                context = endpoint(**values)
+                if inspect.isawaitable(context):
+                    context = await context
+                if context is None:
+                    context = {}
+
+                context["request"] = request
+                return self.TemplateResponse(template_name, context)
+
+            wrapper.__signature__ = signature
+
+            return wrapper
+
+        return decorator
+
+
+templates = Templates(directory=Path(__file__).parent / "templates")
+
+
+def common_parameters(request: "Request"):
+    print(request)
+    return {"a": "b"}
 
 
 @router.get("/")
-def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@templates("index.html")
+def index():
+    pass
 
 
 @router.post("/clicked")
@@ -41,18 +99,47 @@ def chats():
     )
 
 
-@router.post("/messages")
-async def messages(prompt: Annotated[str, Form()]):
-    return f7l.p(f"You asked: {prompt}")
+@router.post("/answer")
+async def answer(prompt: Annotated[str, Form()], background_tasks: BackgroundTasks):
+    answer_id = str(uuid.uuid4())
+    background_tasks.add_task(get_answer, prompt, answer_id)
+    return [
+        f7l.div([f7l.b("User: "), prompt], id=str(uuid.uuid4())),
+        f7l.div(
+            f7l.b("Assistant: "),
+            id=answer_id,
+            sse_swap=answer_id,
+            hx_swap="beforeend",
+        ),
+    ]
 
 
-@router.get("/event-source")
-async def stream():
-    async def gen():
-        message = ""
-        for chunk in ["foo", "bar", "baz"]:
-            await asyncio.sleep(1)
-            message += chunk
-            yield sse_starlette.ServerSentEvent(data=f7l.render(f7l.p(message)))
+async def get_answer(prompt, id):
+    async for chunk in assistant(prompt):
+        await EVENTS.put({"event": id, "data": chunk})
 
-    return sse_starlette.EventSourceResponse(gen())
+
+import string
+
+
+async def assistant(prompt):
+    await asyncio.sleep(random.uniform(0, 1))
+    for c in string.ascii_letters:
+        await asyncio.sleep(random.uniform(0, 200e-3))
+        yield c
+
+
+EVENTS: asyncio.Queue = None
+
+
+@router.get("/events")
+async def events():
+    global EVENTS
+    EVENTS = asyncio.Queue()
+
+    async def event_stream():
+        while True:
+            event = await EVENTS.get()
+            yield event
+
+    return sse_starlette.EventSourceResponse(event_stream())
