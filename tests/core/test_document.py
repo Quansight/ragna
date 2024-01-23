2dc7b51493a871e3c93edb6529e9047e1bbbf849
Davide Di Grande <davidedigrande@rwsdigital.com>
2024-01-23 08:24:24 +0100

    Add pptx functionality tests

--- tests/core/test_document.py
+++ tests/core/test_document.py
@@ -1,6 +1,7 @@
 import docx
+import pptx
 
-from ragna.core import DocxDocumentHandler, LocalDocument
+from ragna.core import DocxDocumentHandler, LocalDocument, PptxDocumentHandler
 
 
 def get_docx_document(tmp_path, docx_text):
@@ -20,3 +21,23 @@ def test_docx(tmp_path):
     assert len(pages) == 2
     for page in pages:
         assert page.text == docx_text
+
+
+def get_pptx_document(tmp_path, pptx_text):
+    document = pptx.Presentation()
+    document.slides.add_slide(document.slide_layouts[0])
+    document.slides[0].shapes.title.text = pptx_text
+    document.slides[0].shapes.add_textbox(0, 0, 100, 100).text = pptx_text
+    path = tmp_path / "test_document.pptx"
+    document.save(path)
+    return LocalDocument.from_path(path)
+
+
+def test_pptx(tmp_path):
+    pptx_text = "ragna is neat!"
+    tmp_pptx_document = get_pptx_document(tmp_path, pptx_text)
+    assert isinstance(tmp_pptx_document.handler, PptxDocumentHandler)
+    pages = list(tmp_pptx_document.extract_pages())
+    assert len(pages) == 2
+    for page in pages:
+        assert page.text == pptx_text
