2f20e6bfb965d53d913ac3afbdd3e4bd63d2561d
Philip Meier <github.pmeier@posteo.de>
2024-01-31 22:04:18 +0100

    add Google assistants (#301)
    Co-authored-by: Nick Byrne <55434794+nenb@users.noreply.github.com>

--- ragna/assistants/__init__.py
+++ ragna/assistants/__init__.py
@@ -1,6 +1,8 @@
 __all__ = [
     "Claude",
     "ClaudeInstant",
+    "GeminiPro",
+    "GeminiUltra",
     "Gpt35Turbo16k",
     "Gpt4",
     "Mpt7bInstruct",
@@ -10,6 +12,7 @@ __all__ = [
 
 from ._anthropic import Claude, ClaudeInstant
 from ._demo import RagnaDemoAssistant
+from ._google import GeminiPro, GeminiUltra
 from ._mosaicml import Mpt7bInstruct, Mpt30bInstruct
 from ._openai import Gpt4, Gpt35Turbo16k
 
