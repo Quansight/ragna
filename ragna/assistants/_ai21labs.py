a7fdd469f89036e8cce3071bf38337c19b8fd120
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-02-01 18:41:35 -0300

    Add `display_name` class method

--- ragna/assistants/_ai21labs.py
+++ ragna/assistants/_ai21labs.py
@@ -14,3 +14,7 @@ class AI21LabsAssistant(ApiAssistant):
     _API_KEY_ENV_VAR = "AI21LABS_API_KEY"
     _MODEL_TYPE: str
     _CONTEXT_SIZE: int = 8_192
+
+    @classmethod
+    def display_name(cls) -> str:
+        return f"AI21Labs/jurassic-2-{cls._MODEL_TYPE}"
