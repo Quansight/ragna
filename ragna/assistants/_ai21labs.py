793fc868ef7cfa6ef524533684ef7595bac54783
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-02-02 19:36:59 -0300

    Rename API key to match AI21 Labs SDK

--- ragna/assistants/_ai21labs.py
+++ ragna/assistants/_ai21labs.py
@@ -6,7 +6,7 @@ from ._api import ApiAssistant
 
 
 class Ai21LabsAssistant(ApiAssistant):
-    _API_KEY_ENV_VAR = "AI21LABS_API_KEY"
+    _API_KEY_ENV_VAR = "AI21_API_KEY"
     _MODEL_TYPE: str
     _CONTEXT_SIZE: int = 8_192
 
