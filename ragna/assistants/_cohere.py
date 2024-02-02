bb94f95434b056d420338b3e0ff5d820413d236c
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-02-02 18:02:54 -0300

    Add `display_name` class method

--- ragna/assistants/_cohere.py
+++ ragna/assistants/_cohere.py
@@ -5,3 +5,7 @@ class CohereApiAssistant(ApiAssistant):
     _API_KEY_ENV_VAR = "COHERE_API_KEY"
     _MODEL: str
     _CONTEXT_SIZE: int
+
+    @classmethod
+    def display_name(cls) -> str:
+        return f"Cohere/{cls._MODEL}"
