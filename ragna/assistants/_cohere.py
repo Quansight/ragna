6bb7b52a2c8a214fb98f3388729564a8c1638f76
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-02-02 20:01:16 -0300

    Add `_call_api` method

--- ragna/assistants/_cohere.py
+++ ragna/assistants/_cohere.py
@@ -1,4 +1,6 @@
-from ragna.core import Source
+from typing import AsyncIterator, cast
+
+from ragna.core import RagnaException, Source
 
 from ._api import ApiAssistant
 
@@ -29,3 +31,27 @@ class CohereApiAssistant(ApiAssistant):
             {"title": source.id, "snippet": source.content} for source in sources
         ]
         return document_sources
+
+    async def _call_api(
+        self, prompt: str, sources: list[Source], *, max_new_tokens: int
+    ) -> AsyncIterator[str]:
+        response = await self._client.post(
+            "https://api.cohere.ai/v1/chat",
+            headers={
+                "accept": "application/json",
+                "content-type": "application/json",
+                "authorization": f"Bearer {self._api_key}",
+            },
+            json={
+                "message": prompt,
+                "model": self._MODEL,
+                "temperature": 0.0,
+                "max_tokens": max_new_tokens,
+                "documents": self._make_source_documents(sources),
+            },
+        )
+        if response.is_error:
+            raise RagnaException(
+                status_code=response.status_code, response=response.json()
+            )
+        yield cast(str, response.json()["text"])
