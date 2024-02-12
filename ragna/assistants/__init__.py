0ab260c2f8d9fd48e196e4aa4577c575d7674068
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-02-12 19:16:07 -0300

    Comment out Jurassic2Mid model until AI21Labs fixes their 500 internal server error

--- ragna/assistants/__init__.py
+++ ragna/assistants/__init__.py
@@ -7,14 +7,17 @@ __all__ = [
     "GeminiUltra",
     "Gpt35Turbo16k",
     "Gpt4",
-    "Jurassic2Mid",
+    # "Jurassic2Mid", # This assistant receives a 500 internal service
+    # error from the remote server, so it should be uncommented in the
+    # future when AI21Labs fixes this
     "Jurassic2Ultra",
     "Mpt7bInstruct",
     "Mpt30bInstruct",
     "RagnaDemoAssistant",
 ]
 
-from ._ai21labs import Jurassic2Mid, Jurassic2Ultra
+# from ._ai21labs import Jurassic2Mid, Jurassic2Ultra
+from ._ai21labs import Jurassic2Ultra
 from ._anthropic import Claude, ClaudeInstant
 from ._cohere import Command, CommandLight
 from ._demo import RagnaDemoAssistant
--- ragna/assistants/_ai21labs.py
+++ ragna/assistants/_ai21labs.py
@@ -62,15 +62,17 @@ class Ai21LabsAssistant(ApiAssistant):
         yield cast(str, response.json()["outputs"][0]["text"])
 
 
-class Jurassic2Mid(Ai21LabsAssistant):
-    """[AI21 Labs Jurassic-2 Mid](https://docs.ai21.com/docs/jurassic-2-models)
+# The following assistant receives a 500 internal service error from the remote
+# server, so it should be uncommented in the future when AI21Labs fixes this
+# class Jurassic2Mid(Ai21LabsAssistant):
+#     """[AI21 Labs Jurassic-2 Mid](https://docs.ai21.com/docs/jurassic-2-models)
 
-    !!! info "Required environment variables"
+#     !!! info "Required environment variables"
 
-        - `AI21_API_KEY`
-    """
+#         - `AI21_API_KEY`
+#     """
 
-    _MODEL_TYPE = "mid"
+#     _MODEL_TYPE = "mid"
 
 
 class Jurassic2Ultra(Ai21LabsAssistant):
