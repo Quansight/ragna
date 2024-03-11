a8a1b1f179bbab2806eb410cfc646e6d72d6569b
Philip Meier <github.pmeier@posteo.de>
2024-03-11 16:32:33 +0100

    try pytest-asyncio rather than custom solution

--- tests/assistants/test_api.py
+++ tests/assistants/test_api.py
@@ -1,30 +1,26 @@
-import asyncio
 import os
 
 import pytest
 
 from ragna import assistants
+from ragna._compat import anext
+from ragna.assistants._api import ApiAssistant
 from ragna.core import RagnaException
 
 API_ASSISTANTS = [
     assistant
     for assistant in assistants.__dict__.values()
     if isinstance(assistant, type)
-    and issubclass(assistant, assistants._api.ApiAssistant)
-    and assistant is not assistants._api.ApiAssistant
+    and issubclass(assistant, ApiAssistant)
+    and assistant is not ApiAssistant
 ]
 
 
-@pytest.mark.parametrize("assistant_cls", API_ASSISTANTS)
-def test_api_call_error_smoke(mocker, assistant_cls):
-    mocker.patch.dict(os.environ, {assistant_cls._API_KEY_ENV_VAR: "SENTINEL"})
+@pytest.mark.parametrize("assistant", API_ASSISTANTS)
+async def test_api_call_error_smoke(mocker, assistant):
+    mocker.patch.dict(os.environ, {assistant._API_KEY_ENV_VAR: "SENTINEL"})
 
-    assistant = assistant_cls()
-
-    async def run():
-        return "".join(
-            [chunk async for chunk in assistant.answer(prompt="?", sources=[])]
-        )
+    chunks = assistant().answer(prompt="?", sources=[])
 
     with pytest.raises(RagnaException, match="API call failed"):
-        asyncio.run(run())
+        await anext(chunks)
