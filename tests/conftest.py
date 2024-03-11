5f09eb544ca95793a292d1a15ddd729ce6f91a1a
Philip Meier <github.pmeier@posteo.de>
2024-03-11 16:55:53 +0100

    try setting event loop policy for windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import asyncio
+import platform
+
 import pytest
 
 import ragna
@@ -10,3 +13,10 @@ def tmp_local_root(tmp_path):
         yield ragna.local_root(tmp_path)
     finally:
         ragna.local_root(old)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def windows_event_loop_policy():
+    # See https://stackoverflow.com/a/66772242
+    if platform.system() == "Windows":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
