754ede68ff45f0e67890b9f783aecf89fdbbc41c
Philip Meier <github.pmeier@posteo.de>
2024-03-11 17:06:05 +0100

    add reason

--- tests/utils.py
+++ tests/utils.py
@@ -2,4 +2,6 @@ import platform
 
 import pytest
 
-skip_on_windows = pytest.mark.skipif(platform.system() == "Windows")
+skip_on_windows = pytest.mark.skipif(
+    platform.system() == "Windows", reason="Test is broken skipped on Windows"
+)
