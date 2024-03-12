0a479c7f1383374fae9d1e4bc719f7e112230db4
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-03-11 21:44:47 -0300

    Add draft docstring
    This is just a first draft what will be iterated on later
    
    Remove small typo

---
+++ docs/tutorials/gallery_adding_more_assistants.py
@@ -0,0 +1,9 @@
+"""
+# Adding More Assistants
+
+Ragna has builtin support for several assistants, but there may be cases where you 
+want to use one that is not currently supported.
+
+This tutorial walks you through creating an assistant for LLMs that are not
+currently supported. 
+"""
