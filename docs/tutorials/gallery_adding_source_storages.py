5601689d954e1f6aa083addee9e2a694067ee0f2
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-03-14 15:53:21 -0300

    Add docstring for tutorial

--- docs/tutorials/gallery_adding_source_storages.py
+++ docs/tutorials/gallery_adding_source_storages.py
@@ -1,5 +1,13 @@
 """
 # Adding Source Storages
 
+Ragna has builtin support for [several source storage types][ragna.source_storages], but there may be cases
+where you want to use one that is not currently supported. 
 
+This tutorial walks you through the basics of adding a source storage that is not currently supported. 
+
+!!! tip
+
+    For organizational purposes, this tutorial is divided into steps, but you may wish to perform
+    Step 2 during Step 1 for manual testing or debugging.
 """
