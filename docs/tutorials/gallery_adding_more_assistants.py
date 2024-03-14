14d2215cafe62f1a4703adb0a3ed1f0048d6282f
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-03-14 00:08:03 -0300

    Add `TutorialAssistant._default_answer` and add clarification to explanations

--- docs/tutorials/gallery_adding_more_assistants.py
+++ docs/tutorials/gallery_adding_more_assistants.py
@@ -6,13 +6,21 @@ want to use one that is not currently supported.
 
 This tutorial walks you through the basics of creating an assistant for LLMs that are not
 currently supported. 
+
+!!! tip
+
+    For organizational purposes, this tutorial is divided into steps, but you may wish to perform
+    Step 2 during Step 1 for manual testing or debugging.
 """
 
 # %%
 # ## Step 1: Write the Assistant
 
 # %%
-# First start by importing some necessary components:
+# For this tutorial, we assume the code in this step is located in the file
+# `ragna/assistants/_tutorial.py`
+#
+# First start by importing some necessary components. Ragna uses the `typing` library:
 
 from typing import Iterator
 
@@ -23,7 +31,10 @@ from typing import Iterator
 from ragna.core import Assistant, Source
 
 # %%
-# Here is the finished product that we will explain below:
+# The main thing to do is to implement the `answer` abstract method. The
+# [`answer`][ragna.core.Assistant.answer] method is where you put the logic to access your LLM.
+# This could call an API directly, call other member functions of your assistant that call an API,
+# or call a local LLM. Ragna is designed to give you that flexibility.
 
 
 class TutorialAssistant(Assistant):
@@ -44,6 +55,17 @@ class TutorialAssistant(Assistant):
         yield self._default_answer(prompt, sources)
 
     def _default_answer(self, prompt: str, sources: list[Source]) -> str:
+        """
+        "Compute" the response to a given prompt. In this case,
+        the "computation" is a default string.
+
+        Args:
+            prompt: Prompt to be answered.
+            sources: Sources to use when answering answer the prompt.
+
+        Returns:
+            Answer.
+        """
         return (
             f"This is a default answer. There were {len(sources)} sources."
             ""
@@ -53,19 +75,19 @@ class TutorialAssistant(Assistant):
 
 
 # %%
-# The [`answer`][ragna.core.Assistant.answer] method is where you put the logic to access your LLM.
-# This could ball an API directly, call other member functions of your assistant that call an API,
-# or call a local LLM. Ragna is designed to give you that flexibility.
+# !!! note
+#     While including the `_default_answer` method seems to obfuscate the code, it is meant
+#     to demonstrate that you can add multiple methods to your assistant that help compute
+#     the response. Examples include calling an external API or preparing the sources to be
+#     used by the model.
 
 # %%
 # ## Step 2: Include the Assistant in Ragna
 
 # %%
-# Once you have created your assistant, you must add it to the system so that it is recognized.
+# Once you have created your assistant, you must add it to the system so that Ragna recognizes it.
 # To do this, add your custom assistant to the `__all__` list in the file
-# `ragna/assistants/__init__.py`, and import it in the same file. An example is shown below
-# with the assumption that our `TutorialAssistant` is located in the file
-# `ragna/assistants/_tutorial`:
+# `ragna/assistants/__init__.py`, and import it in the same file. An example is shown below:
 
 # %%
 # ```
@@ -84,5 +106,6 @@ class TutorialAssistant(Assistant):
 # ```
 
 # %%
-# Although it is not a strict requirement, it is a convention that the items added to
-# `ragna/assistants/__init__.py` appear in alphabetical order.
+# !!! note
+#     Although it is not a strict requirement, it is a convention that the items added to
+#     `ragna/assistants/__init__.py` appear in alphabetical order.
