9d84a78c8c8be890e5b4531209b8634f19ba3ff4
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-03-14 23:27:05 -0300

    Add "finished" product to explain later

--- docs/tutorials/gallery_adding_source_storages.py
+++ docs/tutorials/gallery_adding_source_storages.py
@@ -5,9 +5,43 @@ Ragna has builtin support for [several source storage types][ragna.source_storag
 where you want to use one that is not currently supported. 
 
 This tutorial walks you through the basics of adding a source storage that is not currently supported. 
-
-!!! tip
-
-    For organizational purposes, this tutorial is divided into steps, but you may wish to perform
-    Step 2 during Step 1 for manual testing or debugging.
 """
+
+# %%
+# ## The Finished Product
+
+# %%
+# Here is the tutorial example printed in its entirety which will be explained below
+
+from ragna.core import Source, SourceStorage
+from ragna.core._document import Document
+
+
+class tutorial_source_storage(SourceStorage):
+    def __init__(self):
+        # import database api
+
+        # set up database
+        ...
+
+    def store(self, documents: list[Document]) -> None:
+        """Store content of documents.
+
+        Args:
+            documents: Documents to store.
+        """
+        for document in documents:
+            # store documents using database api
+            ...
+
+    def retrieve(self, documents: list[Document], prompt: str) -> list[Source]:
+        """Retrieve sources for a given prompt.
+
+        Args:
+            documents: Documents to retrieve sources from.
+            prompt: Prompt to retrieve sources for.
+
+        Returns:
+            Matching sources for the given prompt ordered by relevance.
+        """
+        return documents
