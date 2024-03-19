901fb64b32f342557c323e760849f13e03063e61
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-03-18 21:34:31 -0300

    Restructure and condense Adding Assistants docs

--- docs/tutorials/gallery_adding_components.py
+++ docs/tutorials/gallery_adding_components.py
@@ -1,15 +1,16 @@
 """
-# Adding an Assistant
+# Adding Components to Ragna
 
-Ragna has builtin support for [several assistants][ragna.assistants], but there may be cases where you 
+Ragna has builtin support for several [assistants][ragna.assistants] and 
+[source storages][ragna.source_storages], but there may be cases where you 
 want to use one that is not currently supported.
 
-This tutorial walks you through the basics of creating an assistant for LLMs that are not
-currently supported. 
+This tutorial walks you through the basics of adding components
+that are not currently officially supported. 
 """
 
 # %%
-# ## The Finished Product
+# ## Adding an LLM Assistant
 
 from typing import Iterator
 
@@ -27,56 +28,18 @@ class TutorialAssistant(Assistant):
 
 
 # %%
-# ## The Explanation
-
-# %%
-# ### Step 0: Import Necessary Modules
-
-# %%
-# First start by importing some necessary components.
-
-# %%
-# Your assistant will subclass the [`Assistant`][ragna.core.Assistant] abstract base class and
-# [`Source`][ragna.core.Source] will be used to hold the documents or files sent to the LLM.
-
-# %%
-# ```python
-# from typing import Iterator
-#
-# from ragna.core import Assistant, Source
-# ```
-
-# %%
-# ### Step 1: Write the Assistant
-
-# %%
-# The main thing to do is to implement the `answer` abstract method. The
-# [`answer`][ragna.core.Assistant.answer] method is where you put the logic to access your LLM.
+# The main thing to do is to implement the [`answer()`][ragna.core.Assistant.answer] abstract method.
+# The [`answer()`][ragna.core.Assistant.answer] method is where you put the logic to access your LLM.
 # This could call an API directly, call other member functions of your assistant that call an API,
 # or call a local LLM. Ragna is designed to give you that flexibility.
 
 # %%
-# Your `answer()` method should take a prompt in the form of a string, and a list of `Source`s, in addition to whatever other arguments you find necessary for your particular assistant. The return type is an `Iterator` of strings.
-
-# %%
-# ```python
-#     def answer(self, prompt: str, sources: list[Source]) -> Iterator[str]:
-#         yield (
-#             f"This is a default answer. There were {len(sources)} sources."
-#             ""
-#             f"The prompt was"
-#             f"{prompt}"
-#         )
-# ```
+# Your [`answer()`][ragna.core.Assistant.answer] method should take a prompt in the form of a
+# string, and a list of [`Source`][ragna.core.Source]s, in addition to whatever other arguments
+# necessary for your particular assistant. The return type is an [`Iterator`](https://docs.python.org/3/library/stdtypes.html#typeiter) of strings.
 
 # %%
 # !!! note
 #     Ragna also supports streaming responses from the assistant. See the
 #     [example how to use streaming responses](../../generated/examples/gallery_streaming.md)
 #     for more information.
-
-# %%
-# See the documentation on [how to include external objects in Ragna](../../references/config.md#referencing-python-objects)
-
-# %%
-# ### Step 2: Use the Assistant in Ragna
