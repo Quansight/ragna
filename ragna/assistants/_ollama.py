a0c84994f992b9b540ae09a75e06acd3b12edd6b
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2024-03-22 17:56:06 -0300

    Add `_make_system_content` method

--- ragna/assistants/_ollama.py
+++ ragna/assistants/_ollama.py
@@ -1,7 +1,7 @@
 # import json
 # from typing import AsyncIterator, cast
 
-# from ragna.core import RagnaException, Source
+from ragna.core import Source  # RagnaException
 
 from ragna.core import Assistant
 
@@ -12,3 +12,11 @@ class OllamaApiAssistant(Assistant):
     @classmethod
     def display_name(cls) -> str:
         return f"Ollama/{cls._MODEL}"
+
+    def _make_system_content(self, sources: list[Source]) -> str:
+        instruction = (
+            "You are an helpful assistants that answers user questions given the context below. "
+            "If you don't know the answer, just say so. Don't try to make up an answer. "
+            "Only use the following sources to generate the answer."
+        )
+        return instruction + "\n\n".join(source.content for source in sources)
