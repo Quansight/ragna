2735298cd51a3eb898fa84ecd837b33a10125baf
Toby Osborne <toby@zagmo.com>
2024-03-25 13:14:30 +1300

    removed SentenceTransformers dependancy for ChromaDB embedding functions

--- ragna/embedding_models/_embedding.py
+++ ragna/embedding_models/_embedding.py
@@ -1,17 +1,14 @@
-from ragna.core import Chunk
-
-from ragna.core._components import Embedding, EmbeddingModel
+from ragna.core import Chunk, Embedding, EmbeddingModel
 
 
 class MiniLML6v2(EmbeddingModel):
-
     def __init__(self):
         super().__init__()
-        from sentence_transformers import SentenceTransformer
-        self.model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+        from chromadb.utils import embedding_functions
+        self.model = embedding_functions.DefaultEmbeddingFunction()
 
     def embed_chunks(self, chunks: list[Chunk]) -> list[Embedding]:
-        return [Embedding(self.embed_text(chunk.text), chunk) for chunk in chunks]
+        return [Embedding(embed_chunk[0], embed_chunk[1]) for embed_chunk in zip(self.embed_text([chunk.text for chunk in chunks]), chunks)]
 
-    def embed_text(self, text: str) -> list[float]:
-        return self.model.encode(text).tolist()
+    def embed_text(self, text: list[str]) -> list[float]:
+        return self.model(text)
