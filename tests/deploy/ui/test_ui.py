e36f4b48dfbbd4ca4a327363f5aacc2e51d74bf4
Blake Rosenthal <blakepdx@protonmail.com>
2024-05-12 13:26:42 -0700

    working health page

--- tests/deploy/ui/test_ui.py
+++ tests/deploy/ui/test_ui.py
@@ -3,6 +3,7 @@ import os
 import pytest
 from fastapi.testclient import TestClient
 from playwright.sync_api import expect, sync_playwright
+from pytest_httpx import HTTPXMock
 
 from ragna.assistants import RagnaDemoAssistant
 from ragna.core._utils import default_user
@@ -40,40 +41,52 @@ class TestAssistant(RagnaDemoAssistant):
             yield content
 
 
+@pytest.fixture(scope="session")
+def headless_mode(pytestconfig):
+    return pytestconfig.getoption("headed") or False
+
+
 @pytest.fixture(scope="function")
 def config(tmp_local_root):
     return Config(local_root=tmp_local_root, assistants=[TestAssistant])
 
 
 @pytest.fixture(scope="function")
-def server(config, open_browser=False):
-    document_root = config.local_root / "documents"
-    document_root.mkdir()
-    document_path = document_root / "test.txt"
-    with open(document_path, "w") as file:
-        file.write("!\n")
-
+def api_client(config):
     with TestClient(
-        api_app(config=config, ignore_unavailable_components=True)
+        api_app(config=config, ignore_unavailable_components=True),
+        base_url=config.api.url,
     ) as client:
         authenticate(client)
 
-        server = ui_app(config=config, open_browser=open_browser)
-        server.serve()
-
-        yield server
+        yield client
 
 
-def test_ui(server) -> None:
+@pytest.fixture(scope="function")
+def server(config, api_client, httpx_mock: HTTPXMock, open_browser=False):
+    # httpx_mock.add_response(url=config.api.url)
+    server = ui_app(config=config, open_browser=open_browser)
+    return server
+
+
+# @pytest.mark.asyncio
+# async def test_ui(config, api_client, server, httpx_mock: HTTPXMock):
+#     httpx_mock.add_response()
+#     server.serve()
+
+
+def test_health_page(config, server, headless_mode) -> None:
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
+        server.serve()
+        browser = playwright.chromium.launch(headless=headless_mode)
         context = browser.new_context()
         page = context.new_page()
-        url = server.origins[0]
+        url = config.ui.origins[0] + "/health"
         page.goto(url)
-        expect(page.get_by_role("button", name="Sign In")).to_be_visible()
-        page.get_by_role("button", name="Sign In").click()
-        expect(page.get_by_role("button", name=" New Chat")).to_be_visible()
+        expect(page.get_by_role("heading", name="Ok")).to_be_visible()
+        # expect(page.get_by_role("button", name="Sign In")).to_be_visible()
+        # page.get_by_role("button", name="Sign In").click()
+        # expect(page.get_by_role("button", name=" New Chat")).to_be_visible()
         # page.locator("div").filter(
         #     has_text=re.compile(r"^Source storageChromaLanceDBRagna/DemoSourceStorage$")
         # ).get_by_role("combobox").select_option("LanceDB")
