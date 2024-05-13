45a7f3342649a013d3a4a15fcaaf4254c9e62db3
Blake Rosenthal <blakepdx@protonmail.com>
2024-05-13 11:18:28 -0700

    working api server

--- tests/deploy/ui/test_ui.py
+++ tests/deploy/ui/test_ui.py
@@ -1,9 +1,12 @@
 import os
+import time
+from multiprocessing import Process
 
+import httpx
 import pytest
+import uvicorn
 from fastapi.testclient import TestClient
 from playwright.sync_api import expect, sync_playwright
-from pytest_httpx import HTTPXMock
 
 from ragna.assistants import RagnaDemoAssistant
 from ragna.core._utils import default_user
@@ -62,29 +65,63 @@ def api_client(config):
         yield client
 
 
+TEST_UI_HOSTNAME = "http://localhost"
+TEST_API_PORT = "8769"
+
+
 @pytest.fixture(scope="function")
-def server(config, api_client, httpx_mock: HTTPXMock, open_browser=False):
-    # httpx_mock.add_response(url=config.api.url)
+def api_server(config):
+    def start_server():
+        uvicorn.run(
+            api_app(
+                config=config,
+                ignore_unavailable_components=True,
+            ),
+            host=config.api.hostname,
+            port=config.api.port,
+        )
+
+    def server_up():
+        try:
+            return httpx.get(config.api.url).is_success
+        except httpx.ConnectError:
+            return False
+
+    proc = Process(target=start_server, args=(), daemon=True)
+    proc.start()
+
+    timeout = 5
+    while timeout < 0 and not server_up():
+        print(f"Waiting for API server to come up on {config.api.url}")
+        time.sleep(1)
+        timeout -= 1
+
+    yield proc
+    proc.kill()
+
+
+@pytest.fixture(scope="function")
+def ui_server(config, api_server, open_browser=False):
     server = ui_app(config=config, open_browser=open_browser)
     return server
 
 
-# @pytest.mark.asyncio
-# async def test_ui(config, api_client, server, httpx_mock: HTTPXMock):
-#     httpx_mock.add_response()
-#     server.serve()
-
-
-def test_health_page(config, server, headless_mode) -> None:
+def test_ui(config, ui_server, headless_mode) -> None:
     with sync_playwright() as playwright:
-        server.serve()
+        ui_server.serve()
+
         browser = playwright.chromium.launch(headless=headless_mode)
         context = browser.new_context()
         page = context.new_page()
-        url = config.ui.origins[0] + "/health"
-        page.goto(url)
+
+        health_url = config.ui.origins[0] + "/health"
+        page.goto(health_url)
         expect(page.get_by_role("heading", name="Ok")).to_be_visible()
-        # expect(page.get_by_role("button", name="Sign In")).to_be_visible()
+
+        index_url = config.ui.origins[0]
+        page.goto(index_url)
+        expect(page.get_by_role("button", name="Sign In")).to_be_visible()
+
         # page.get_by_role("button", name="Sign In").click()
         # expect(page.get_by_role("button", name=" New Chat")).to_be_visible()
         # page.locator("div").filter(
@@ -93,6 +130,5 @@ def test_health_page(config, server, headless_mode) -> None:
         # page.get_by_role("button", name="Advanced Configurations ▶").click()
         # page.locator("#fileUpload-p2365").click()
 
-        # ---------------------
         context.close()
         browser.close()
