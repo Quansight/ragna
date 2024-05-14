c6f21537130e30aac1c7672c98e4690de875980b
Blake Rosenthal <blakepdx@protonmail.com>
2024-05-13 18:21:46 -0700

    add more tests, auth header not working

--- tests/deploy/ui/test_ui.py
+++ tests/deploy/ui/test_ui.py
@@ -3,9 +3,9 @@ import time
 from multiprocessing import Process
 
 import httpx
+import panel as pn
 import pytest
 import uvicorn
-from fastapi.testclient import TestClient
 from playwright.sync_api import expect, sync_playwright
 
 from ragna.assistants import RagnaDemoAssistant
@@ -13,24 +13,6 @@ from ragna.core._utils import default_user
 from ragna.deploy import Config
 from ragna.deploy._api import app as api_app
 from ragna.deploy._ui import app as ui_app
-
-
-def authenticate(client: TestClient) -> None:
-    username = default_user()
-    token = (
-        client.post(
-            "/token",
-            data={
-                "username": username,
-                "password": os.environ.get(
-                    "RAGNA_DEMO_AUTHENTICATION_PASSWORD", username
-                ),
-            },
-        )
-        .raise_for_status()
-        .json()
-    )
-    client.headers["Authorization"] = f"Bearer {token}"
 
 
 class TestAssistant(RagnaDemoAssistant):
@@ -49,7 +31,7 @@ def headed_mode(pytestconfig):
     return pytestconfig.getoption("headed") or False
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def config(tmp_local_root):
     return Config(local_root=tmp_local_root, assistants=[TestAssistant])
 
@@ -58,7 +40,7 @@ TEST_UI_HOSTNAME = "http://localhost"
 TEST_API_PORT = "8769"
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def api_server(config):
     def start_server():
         uvicorn.run(
@@ -89,35 +71,99 @@ def api_server(config):
     proc.kill()
 
 
+def auth_header(base_url):
+    username = default_user()
+    token = (
+        httpx.post(
+            base_url + "/token",
+            data={
+                "username": username,
+                "password": os.environ.get(
+                    "RAGNA_DEMO_AUTHENTICATION_PASSWORD", username
+                ),
+            },
+        )
+        .raise_for_status()
+        .json()
+    )
+
+    return f"Bearer {token}"
+
+
 @pytest.fixture(scope="function")
-def ui_server(config, api_server):
+def page(config, api_server, headed_mode):
     server = ui_app(config=config, open_browser=False)
-    return server
 
-
-def test_ui(config, ui_server, headed_mode) -> None:
     with sync_playwright() as playwright:
-        ui_server.serve()
-
+        server.serve()
         browser = playwright.chromium.launch(headless=not headed_mode)
         context = browser.new_context()
         page = context.new_page()
 
-        health_url = config.ui.origins[0] + "/health"
-        page.goto(health_url)
-        expect(page.get_by_role("heading", name="Ok")).to_be_visible()
-
-        index_url = config.ui.origins[0]
-        page.goto(index_url)
-        expect(page.get_by_role("button", name="Sign In")).to_be_visible()
-
-        # page.get_by_role("button", name="Sign In").click()
-        # expect(page.get_by_role("button", name=" New Chat")).to_be_visible()
-        # page.locator("div").filter(
-        #     has_text=re.compile(r"^Source storageChromaLanceDBRagna/DemoSourceStorage$")
-        # ).get_by_role("combobox").select_option("LanceDB")
-        # page.get_by_role("button", name="Advanced Configurations ▶").click()
-        # page.locator("#fileUpload-p2365").click()
+        yield page
 
         context.close()
         browser.close()
+        pn.state.kill_all_servers()
+
+
+def test_health(config, page) -> None:
+    health_url = config.ui.origins[0] + "/health"
+    page.goto(health_url)
+    expect(page.get_by_role("heading", name="Ok")).to_be_visible()
+
+
+def test_index_with_blank_credentials(config, page) -> None:
+    # Index page, no auth
+    index_url = config.ui.origins[0]
+    page.goto(index_url)
+    expect(page.get_by_role("button", name="Sign In")).to_be_visible()
+
+    # Authorize with no credentials
+    page.get_by_role("button", name="Sign In").click()
+    expect(page.get_by_role("button", name=" New Chat")).to_be_visible()
+
+
+def test_index_with_auth_header(config, page) -> None:
+    pn.state.headers["Authorization"] = auth_header(config.api.url)
+
+    index_url = config.ui.origins[0]
+    page.goto(index_url)
+    expect(page.get_by_role("button", name=" New Chat")).to_be_visible()
+
+    # page.locator("div").filter(
+    #     has_text=re.compile(r"^Source storageChromaLanceDBRagna/DemoSourceStorage$")
+    # ).get_by_role("combobox").select_option("LanceDB")
+    # page.get_by_role("button", name="Advanced Configurations ▶").click()
+    # page.locator("#fileUpload-p2365").click()
+
+
+# def test_ui_with_auth_headers(config, ui_server, headed_mode) -> None:
+#     with sync_playwright() as playwright:
+#
+#         browser = playwright.chromium.launch(headless=not headed_mode)
+#         context = browser.new_context()
+#         page = context.new_page()
+#
+#         # Health page
+#         health_url = config.ui.origins[0] + "/health"
+#         page.goto(health_url)
+#         expect(page.get_by_role("heading", name="Ok")).to_be_visible()
+#
+#         # Index page, no auth
+#         index_url = config.ui.origins[0]
+#         page.goto(index_url)
+#         expect(page.get_by_role("button", name="Sign In")).to_be_visible()
+#
+#         # Authorize with no credentials
+#         page.get_by_role("button", name="Sign In").click()
+#         expect(page.get_by_role("button", name=" New Chat")).to_be_visible()
+#
+#         # page.locator("div").filter(
+#         #     has_text=re.compile(r"^Source storageChromaLanceDBRagna/DemoSourceStorage$")
+#         # ).get_by_role("combobox").select_option("LanceDB")
+#         # page.get_by_role("button", name="Advanced Configurations ▶").click()
+#         # page.locator("#fileUpload-p2365").click()
+#
+#         context.close()
+#         browser.close()
