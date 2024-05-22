d9f19cbbd4236731988909903a5049d18101bcb7
Blake Rosenthal <blakepdx@protonmail.com>
2024-05-22 09:59:04 -0700

    wip: replace server with cli function

--- tests/deploy/ui/test_ui.py
+++ tests/deploy/ui/test_ui.py
@@ -1,17 +1,15 @@
 import socket
+import subprocess
+import sys
 import time
-from multiprocessing import Process
 
 import httpx
 import panel as pn
 import pytest
-import uvicorn
 from playwright.sync_api import expect, sync_playwright
 
 from ragna._utils import timeout_after
 from ragna.deploy import Config
-from ragna.deploy._api import app as api_app
-from ragna.deploy._ui import app as ui_app
 
 from ..utils import TestAssistant
 
@@ -27,53 +25,74 @@ def headed_mode(pytestconfig):
     return pytestconfig.getoption("headed") or False
 
 
+@pytest.fixture(scope="session")
+def ui_port():
+    return get_available_port()
+
+
+@pytest.fixture(scope="session")
+def api_port():
+    return get_available_port()
+
+
 @pytest.fixture
-def config(tmp_local_root):
-    ui_port = get_available_port()
-    api_port = get_available_port()
-    return Config(
+def config(tmp_local_root, ui_port, api_port):
+    config = Config(
         local_root=tmp_local_root,
         assistants=[TestAssistant],
         ui=dict(port=ui_port),
         api=dict(port=api_port),
     )
+    path = tmp_local_root / "ragna.toml"
+    config.to_file(path)
+    return config
 
 
-class ApiServer:
-    def __init__(self, config):
+class Server:
+    def __init__(self, config, base_url):
         self.config = config
-
-    def start_server(self):
-        uvicorn.run(
-            api_app(
-                config=self.config,
-                ignore_unavailable_components=True,
-            ),
-            host=self.config.api.hostname,
-            port=self.config.api.port,
-        )
+        self.base_url = base_url
 
     def server_up(self):
         try:
-            return httpx.get(self.config.api.url).is_success
+            return httpx.get(self.base_url).is_success
         except httpx.ConnectError:
             return False
 
     @timeout_after(5)
     def start(self):
-        self.proc = Process(target=self.start_server, args=(), daemon=True)
-        self.proc.start()
+        self.proc = subprocess.Popen(
+            [
+                sys.executable,
+                "-m",
+                "ragna",
+                "ui",
+                "--config",
+                self.config.local_root / "ragna.toml",
+                "--start-api",
+                "--ignore-unavailable-components",
+                "--no-open-browser",
+            ],
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+        )
 
         while not self.server_up():
             time.sleep(1)
 
     def stop(self):
-        self.proc.terminate()
+        self.proc.kill()
+        pn.state.kill_all_servers()
 
 
 @pytest.fixture
-def api_server(config):
-    server = ApiServer(config)
+def base_ui_url(ui_port):
+    return f"http://127.0.0.1:{ui_port}"
+
+
+@pytest.fixture
+def server(config, base_ui_url):
+    server = Server(config, base_ui_url)
     try:
         server.start()
         yield server
@@ -82,16 +101,8 @@ def api_server(config):
 
 
 @pytest.fixture
-def base_ui_url(config):
-    return f"http://{config.ui.hostname}:{config.ui.port}"
-
-
-@pytest.fixture
-def context(config, api_server, headed_mode):
-    server = ui_app(config=config, open_browser=False)
-
+def context(server, headed_mode):
     with sync_playwright() as playwright:
-        server.serve()
         browser = playwright.chromium.launch(headless=not headed_mode)
         context = browser.new_context()
 
@@ -99,7 +110,6 @@ def context(config, api_server, headed_mode):
 
         context.close()
         browser.close()
-        pn.state.kill_all_servers()
 
 
 def test_health(base_ui_url, context) -> None:
@@ -117,8 +127,6 @@ def test_index(base_ui_url, context, config) -> None:
     expect(page.get_by_role("button", name="Sign In")).to_be_visible()
 
     # Authorize with no credentials
-    # page.locator('input[type="text"]').fill("hello")
-    # page.locator('input[type="password"]').fill("hello")
     page.get_by_role("button", name="Sign In").click()
     expect(page.get_by_role("button", name=" New Chat")).to_be_visible()
 
@@ -166,29 +174,11 @@ def test_index(base_ui_url, context, config) -> None:
     # chat_box = page.get_by_placeholder("Ask a question about the")
     # expect(chat_box).to_be_visible()
 
-
-@pytest.mark.skip(reason="TODO: figure out best locators")
-def test_new_chat(config, page) -> None:
-    index_url = config.ui.origins[0]
-    page.goto(index_url)
-    expect(page.get_by_role("button", name="Sign In")).to_be_visible()
-    page.get_by_role("button", name="Sign In").click()
-
-    expect(page.get_by_role("button", name=" New Chat")).to_be_visible()
-    page.get_by_role("button", name=" New Chat").click()
-
-    expect(page.locator("#fileUpload-p4447")).to_be_visible()
-    page.locator("#fileUpload-p4447").click()
-
-    page.locator("#fileUpload-p4447").set_input_files()
-    page.get_by_role("button", name="Start Conversation").click()
-    page.get_by_text("How can I help you with the").click()
-    page.get_by_placeholder("Ask a question about the").click()
-    page.get_by_placeholder("Ask a question about the").fill(
-        "Tell me about the documents"
-    )
-    page.get_by_role("button", name="").click()
-    page.get_by_role("button", name=" Source Info").click()
-    page.locator("#main div").filter(has_text="Source Info ¶ This response").nth(
-        3
-    ).click()
+    # page.get_by_placeholder("Ask a question about the").fill(
+    #     "Tell me about the documents"
+    # )
+    # page.get_by_role("button", name="").click()
+    # page.get_by_role("button", name=" Source Info").click()
+    # page.locator("#main div").filter(has_text="Source Info ¶ This response").nth(
+    #     3
+    # ).click()
