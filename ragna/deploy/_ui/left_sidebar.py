00d9c028ee141c55d9c9a1413155c9fece3c6557
arjxn.py <arjxn.py@gmail.com>
2024-07-09 05:15:15 +0530

    #391 Chat Ordering from Newest to Oldest

--- ragna/deploy/_ui/left_sidebar.py
+++ ragna/deploy/_ui/left_sidebar.py
@@ -60,7 +60,7 @@ class LeftSidebar(pn.viewable.Viewer):
     @pn.depends("refresh_counter", "chats", "current_chat_id", on_init=True)
     def __panel__(self):
         self.chat_buttons = []
-        for chat in self.chats:
+        for chat in reversed(self.chats):
             button = pn.widgets.Button(
                 name=chat["metadata"]["name"],
                 css_classes=["chat_button"],
