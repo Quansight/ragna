67de21b13787054a1ff834d933558f3ae9488e1b
arjxn.py <arjxn.py@gmail.com>
2024-07-12 04:57:52 +0530

    Linter being linter :sweat_smile:

--- ragna/deploy/_ui/left_sidebar.py
+++ ragna/deploy/_ui/left_sidebar.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import panel as pn
 import param
 
@@ -59,13 +61,12 @@ class LeftSidebar(pn.viewable.Viewer):
 
     @pn.depends("refresh_counter", "chats", "current_chat_id", on_init=True)
     def __panel__(self):
-        from datetime import datetime
         epoch = datetime(1970, 1, 1)
         self.chats.sort(
             key=lambda chat: (
                 epoch if not chat["messages"] else chat["messages"][-1]["timestamp"]
             ),
-            reverse=True
+            reverse=True,
         )
 
         self.chat_buttons = []
