13b1c0f72fd6a317016524667d00a7aa9bd0d645
Blake R <blakepdx@protonmail.com>
2024-08-08 05:49:21 -0700

    Use MetadataFilter in LanceDB (#461)
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>
    Co-authored-by: Nick Byrne <byrnen8@tcd.ie>

--- ragna/source_storages/_lancedb.py
+++ ragna/source_storages/_lancedb.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import uuid
-from typing import cast
+from collections import defaultdict
+from typing import TYPE_CHECKING, Optional, cast
 
 import ragna
 from ragna.core import (
@@ -7,11 +10,15 @@ from ragna.core import (
     MetadataFilter,
     MetadataOperator,
     PackageRequirement,
+    RagnaException,
     Requirement,
     Source,
 )
 
 from ._vector_database import VectorDatabaseSourceStorage
+
+if TYPE_CHECKING:
+    import lancedb
 
 
 class LanceDB(VectorDatabaseSourceStorage):
@@ -40,64 +47,115 @@ class LanceDB(VectorDatabaseSourceStorage):
         super().__init__()
 
         import lancedb
-        import pyarrow as pa
 
         self._db = lancedb.connect(ragna.local_root() / "lancedb")
-        # Create schema at runtime!
-        self._schema = pa.schema(
-            [
-                pa.field("id", pa.string()),
-                pa.field("document_id", pa.string()),
-                pa.field("page_numbers", pa.string()),
-                pa.field("text", pa.string()),
-                pa.field(
-                    self._VECTOR_COLUMN_NAME,
-                    pa.list_(pa.float32(), self._embedding_dimensions),
-                ),
-                pa.field("num_tokens", pa.int32()),
-            ]
-        )
 
     _VECTOR_COLUMN_NAME = "embedded_text"
+
+    def _get_table(self, corpus_name: Optional[str] = None) -> lancedb.table.Table:
+        if corpus_name is None:
+            corpus_name = self._embedding_id
+
+        if corpus_name in self._db.table_names():
+            return self._db.open_table(corpus_name)
+        else:
+            import pyarrow as pa
+
+            return self._db.create_table(
+                name=corpus_name,
+                schema=pa.schema(
+                    [
+                        pa.field("id", pa.string()),
+                        pa.field("document_id", pa.string()),
+                        pa.field("document_name", pa.string()),
+                        pa.field("page_numbers", pa.string()),
+                        pa.field("text", pa.string()),
+                        pa.field(
+                            self._VECTOR_COLUMN_NAME,
+                            pa.list_(pa.float32(), self._embedding_dimensions),
+                        ),
+                        pa.field("num_tokens", pa.int32()),
+                    ]
+                ),
+            )
+
+    _PYTHON_TO_LANCE_TYPE_MAP = {
+        bool: "boolean",
+        int: "int",
+        float: "float",
+        str: "string",
+    }
 
     def store(
         self,
         documents: list[Document],
         *,
+        corpus_name: Optional[str] = None,
         chunk_size: int = 500,
         chunk_overlap: int = 250,
     ) -> None:
-        table = self._db.create_table(name=self._embedding_id, schema=self._schema)
+        table = self._get_table(corpus_name)
 
+        document_field_types = defaultdict(set)
         for document in documents:
-            for chunk in self._chunk_pages(
-                document.extract_pages(),
-                chunk_size=chunk_size,
-                chunk_overlap=chunk_overlap,
-            ):
-                table.add(
-                    [
-                        {
-                            "id": str(uuid.uuid4()),
-                            "document_id": str(document.id),
-                            "document_name": str(document.name),
-                            "page_numbers": self._page_numbers_to_str(
-                                chunk.page_numbers
-                            ),
-                            "text": chunk.text,
-                            self._VECTOR_COLUMN_NAME: self._embedding_function(
-                                [chunk.text]
-                            )[0],
-                            "num_tokens": chunk.num_tokens,
-                        }
-                    ]
+            for field, value in document.metadata.items():
+                document_field_types[field].add(type(value))
+
+        document_fields = {}
+        for field, types in document_field_types.items():
+            if len(types) > 1:
+                raise RagnaException(
+                    "Multiple types for metadata value", key=field, types=sorted(types)
                 )
+            document_fields[field] = self._PYTHON_TO_LANCE_TYPE_MAP[types.pop()]
+
+        schema_fields = set(table.schema.names)
+
+        missing_fields = document_fields.keys() - schema_fields
+        if missing_fields:
+            # Unfortunately, LanceDB does not support adding columns with a specific
+            # type, but the the type is automatically inferred from the value.
+            table.add_columns(
+                {
+                    field: f"CAST(NULL as {document_fields[field]})"
+                    for field in missing_fields
+                }
+            )
+
+        default_metadata = {
+            field: None for field in document_fields.keys() | schema_fields
+        }
+
+        table.add(
+            [
+                {
+                    # Unpacking the default metadata first so it can be
+                    # overridden by concrete values if present
+                    **default_metadata,
+                    **document.metadata,
+                    "id": str(uuid.uuid4()),
+                    "document_id": str(document.id),
+                    "document_name": str(document.name),
+                    "page_numbers": self._page_numbers_to_str(chunk.page_numbers),
+                    "text": chunk.text,
+                    self._VECTOR_COLUMN_NAME: self._embedding_function([chunk.text])[0],
+                    "num_tokens": chunk.num_tokens,
+                }
+                for document in documents
+                for chunk in self._chunk_pages(
+                    document.extract_pages(),
+                    chunk_size=chunk_size,
+                    chunk_overlap=chunk_overlap,
+                )
+            ]
+        )
 
     # https://lancedb.github.io/lancedb/sql/
     _METADATA_OPERATOR_MAP = {
         MetadataOperator.AND: "AND",
         MetadataOperator.OR: "OR",
         MetadataOperator.EQ: "=",
+        MetadataOperator.NE: "!=",
         MetadataOperator.LT: "<",
         MetadataOperator.LE: "<=",
         MetadataOperator.GT: ">",
@@ -112,53 +170,60 @@ class LanceDB(VectorDatabaseSourceStorage):
             MetadataOperator.AND,
             MetadataOperator.OR,
         }:
-            return f" {self._METADATA_OPERATOR_MAP[metadata_filter.operator]} ".join(
+            operator = f" {self._METADATA_OPERATOR_MAP[metadata_filter.operator]} "
+            return operator.join(
                 f"({self._translate_metadata_filter(child)})"
                 for child in metadata_filter.value
             )
-        elif metadata_filter.operator is MetadataOperator.NE:
-            return f"NOT ({self._translate_metadata_filter(MetadataFilter.eq(metadata_filter.key, metadata_filter.value))})"
         elif metadata_filter.operator is MetadataOperator.NOT_IN:
-            return f"NOT ({self._translate_metadata_filter(MetadataFilter.in_(metadata_filter.key, metadata_filter.value))})"
+            in_ = self._translate_metadata_filter(
+                MetadataFilter.in_(metadata_filter.key, metadata_filter.value)
+            )
+            return f"NOT ({in_})"
         else:
+            key = metadata_filter.key
+            operator = self._METADATA_OPERATOR_MAP[metadata_filter.operator]
             value = (
                 tuple(metadata_filter.value)
                 if metadata_filter.operator is MetadataOperator.IN
                 else metadata_filter.value
             )
-            return f"{metadata_filter.key} {self._METADATA_OPERATOR_MAP[metadata_filter.operator]} {value!r}"
+            return f"{key} {operator} {value!r}"
 
     def retrieve(
         self,
-        documents: list[Document],
+        metadata_filter: Optional[MetadataFilter],
         prompt: str,
         *,
-        chat_id: uuid.UUID,
+        corpus_name: Optional[str] = None,
         chunk_size: int = 500,
         num_tokens: int = 1024,
     ) -> list[Source]:
-        table = self._db.open_table(str(chat_id))
+        table = self._get_table(corpus_name)
 
         # We cannot retrieve source by a maximum number of tokens. Thus, we estimate how
         # many sources we have to query. We overestimate by a factor of two to avoid
-        # retrieving to few sources and needed to query again.
+        # retrieving too few sources and needing to query again.
         limit = int(num_tokens * 2 / chunk_size)
-        results = (
-            table.search(
-                self._embedding_function([prompt])[0],
-                vector_column_name=self._VECTOR_COLUMN_NAME,
-            )
-            .limit(limit)
-            .to_arrow()
+
+        search = table.search(
+            self._embedding_function([prompt])[0],
+            vector_column_name=self._VECTOR_COLUMN_NAME,
         )
 
-        document_map = {str(document.id): document for document in documents}
+        if metadata_filter:
+            search = search.where(
+                self._translate_metadata_filter(metadata_filter), prefilter=True
+            )
+
+        results = search.limit(limit).to_arrow()
+
         return self._take_sources_up_to_max_tokens(
             (
                 Source(
                     id=result["id"],
-                    document_id=uuid.UUID(result["document_id"]),
-                    document_name=document_map[result["document_id"]].name,
+                    document_id=result["document_id"],
+                    document_name=result["document_name"],
                     # For some reason adding an empty string during store() results
                     # in this field being None. Thus, we need to parse it back here.
                     # TODO: See if there is a configuration option for this
