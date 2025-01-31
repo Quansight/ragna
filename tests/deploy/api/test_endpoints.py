773a0308eb34a61e08c3abe1e316506d6615f7bc
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2025-01-30 20:44:10 -0800

    Add test for `GET document`

--- tests/deploy/api/test_endpoints.py
+++ tests/deploy/api/test_endpoints.py
@@ -56,3 +56,36 @@ def test_get_documents(tmp_local_root):
         assert sorted(documents, key=_sorting_key) == sorted(
             response.json(), key=_sorting_key
         )
+
+
+def test_get_document(tmp_local_root):
+    config = Config(local_root=tmp_local_root)
+
+    document_root = config.local_root / "documents"
+    document_root.mkdir(exist_ok=True)
+    document_path = document_root / "test_get_document.txt"
+    with open(document_path, "w") as file:
+        file.write("Needs more reverb\n")
+
+    with make_api_client(
+        config=Config(), ignore_unavailable_components=False
+    ) as client:
+        document = (
+            client.post(
+                "/api/documents",
+                json=[{"name": document_path.name}],
+            )
+            .raise_for_status()
+            .json()[0]
+        )
+
+        with open(document_path, "rb") as file:
+            client.put(
+                "/api/documents",
+                files={"documents": [(document["id"], file)]},
+            )
+
+        response = client.get(f"/api/documents/{document['id']}")
+        response.raise_for_status()
+
+        assert document == response.json()
