22a31b679ed783830a65228b0462b70b0b2a6d80
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2025-02-01 03:28:10 -0800

    Store document text content in a variable to be reused

--- tests/deploy/api/test_endpoints.py
+++ tests/deploy/api/test_endpoints.py
@@ -2,19 +2,22 @@ from ragna.deploy import Config
 from tests.deploy.api.utils import upload_documents
 from tests.deploy.utils import make_api_client
 
+_document_content_text = [
+    f"Needs more {needs_more_of}\n" for needs_more_of in ["reverb", "cowbell"]
+]
+
 
 def test_get_documents(tmp_local_root):
     config = Config(local_root=tmp_local_root)
-    needs_more_of = ["reverb", "cowbell"]
 
     document_root = config.local_root / "documents"
     document_root.mkdir()
     document_paths = [
-        document_root / f"test{counter}.txt" for counter in range(len(needs_more_of))
+        document_root / f"test{idx}.txt" for idx in range(len(_document_content_text))
     ]
-    for what_it_needs, document_path in zip(needs_more_of, document_paths):
+    for content, document_path in zip(_document_content_text, document_paths):
         with open(document_path, "w") as file:
-            file.write(f"Needs more {what_it_needs}\n")
+            file.write(content)
 
     with make_api_client(
         config=Config(), ignore_unavailable_components=False
@@ -38,7 +41,7 @@ def test_get_document(tmp_local_root):
     document_root.mkdir()
     document_path = document_root / "test.txt"
     with open(document_path, "w") as file:
-        file.write("Needs more reverb\n")
+        file.write(_document_content_text[0])
 
     with make_api_client(
         config=Config(), ignore_unavailable_components=False
@@ -55,8 +58,9 @@ def test_get_document_content(tmp_local_root):
     document_root = config.local_root / "documents"
     document_root.mkdir()
     document_path = document_root / "test.txt"
+    document_content = _document_content_text[0]
     with open(document_path, "w") as file:
-        file.write("Needs more reverb\n")
+        file.write(document_content)
 
     with make_api_client(
         config=Config(), ignore_unavailable_components=False
@@ -68,4 +72,4 @@ def test_get_document_content(tmp_local_root):
         ) as response:
             received_lines = list(response.iter_lines())
 
-    assert received_lines == ["Needs more reverb"]
+    assert received_lines == [document_content.replace("\n", "")]
