6abfb0090d258dfe0050bef4e9ba293392b3b040
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2025-01-31 16:36:05 -0800

    Remove unique names where appropriate

--- tests/deploy/api/test_endpoints.py
+++ tests/deploy/api/test_endpoints.py
@@ -10,10 +10,9 @@ def test_get_documents(tmp_local_root):
     needs_more_of = ["reverb", "cowbell"]
 
     document_root = config.local_root / "documents"
-    document_root.mkdir(exist_ok=True)
+    document_root.mkdir()
     document_paths = [
-        document_root / f"test_get_documents_{what_it_needs}.txt"
-        for what_it_needs in needs_more_of
+        document_root / f"test{counter}.txt" for counter in range(len(needs_more_of))
     ]
     for what_it_needs, document_path in zip(needs_more_of, document_paths):
         with open(document_path, "w") as file:
@@ -59,8 +58,8 @@ def test_get_document(tmp_local_root):
     config = Config(local_root=tmp_local_root)
 
     document_root = config.local_root / "documents"
-    document_root.mkdir(exist_ok=True)
-    document_path = document_root / "test_get_document.txt"
+    document_root.mkdir()
+    document_path = document_root / "test.txt"
     with open(document_path, "w") as file:
         file.write("Needs more reverb\n")
 
@@ -91,8 +90,8 @@ def test_get_document_content(tmp_local_root):
     config = Config(local_root=tmp_local_root)
 
     document_root = config.local_root / "documents"
-    document_root.mkdir(exist_ok=True)
-    document_path = document_root / "test_get_document_content.txt"
+    document_root.mkdir()
+    document_path = document_root / "test.txt"
     with open(document_path, "w") as file:
         file.write("Needs more reverb\n")
 
