0d116bc7885983143f4ad25b7a526d6bc0e620fe
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2025-04-09 19:56:39 -0700

    Revert "Mark methods as `async` in abstract base class"
    This reverts commit a588aa9c216537eee8b4bebab7f807e769c309a7.

--- ragna/core/_components.py
+++ ragna/core/_components.py
@@ -133,7 +133,7 @@ class SourceStorage(Component, abc.ABC):
     __ragna_protocol_methods__ = ["store", "retrieve"]
 
     @abc.abstractmethod
-    async def store(self, corpus_name: str, documents: list[Document]) -> None:
+    def store(self, corpus_name: str, documents: list[Document]) -> None:
         """Store content of documents.
 
         Args:
@@ -143,7 +143,7 @@ class SourceStorage(Component, abc.ABC):
         ...
 
     @abc.abstractmethod
-    async def retrieve(
+    def retrieve(
         self, corpus_name: str, metadata_filter: MetadataFilter, prompt: str
     ) -> list[Source]:
         """Retrieve sources for a given prompt.
@@ -158,7 +158,7 @@ class SourceStorage(Component, abc.ABC):
         """
         ...
 
-    async def list_corpuses(self) -> list[str]:
+    def list_corpuses(self) -> list[str]:
         """List available corpuses.
 
         Returns:
@@ -171,7 +171,7 @@ class SourceStorage(Component, abc.ABC):
             http_detail=RagnaException.MESSAGE,
         )
 
-    async def list_metadata(
+    def list_metadata(
         self, corpus_name: Optional[str] = None
     ) -> dict[str, dict[str, tuple[str, list[Any]]]]:
         """List available metadata for corpuses.
