af99c037a6cf5691268aae76053d19e0f86702cc
Philip Meier <github.pmeier@posteo.de>
2025-06-20 08:54:37 +0200

    cleanup config (#593)

--- ragna/deploy/_config.py
+++ ragna/deploy/_config.py
@@ -2,24 +2,17 @@ from __future__ import annotations
 
 import itertools
 from pathlib import Path
-from typing import (
-    Annotated,
-    Any,
-    Callable,
-    Generic,
-    Type,
-    TypeVar,
-    Union,
-)
+from typing import Annotated, ClassVar, Type, Union, cast
 
 import tomlkit
 import tomlkit.container
 import tomlkit.items
-from pydantic import AfterValidator, Field, ImportString, model_validator
+from pydantic import AfterValidator, Field, ImportString
 from pydantic_settings import (
     BaseSettings,
     PydanticBaseSettingsSource,
     SettingsConfigDict,
+    TomlConfigSettingsSource,
 )
 
 import ragna
@@ -29,35 +22,11 @@ from ragna.core import Assistant, Document, RagnaException, SourceStorage
 from ._auth import Auth
 from ._key_value_store import KeyValueStore
 
-T = TypeVar("T")
-
-
-class AfterConfigValidateDefault(Generic[T]):
-    """This class exists for a specific use case:
-
-    - We have values for which we need the validated config to compute the default,
-      e.g. the API default origins can only be computed after we know the UI hostname
-      and port.
-    - We want to use the plain annotations rather than allowing a sentinel type, e.g.
-      `str` vs. `Optional[str]`.
-    """
-
-    def __init__(self, make_default: Callable[[Config], T]) -> None:
-        self.make_default = make_default
-
-    @classmethod
-    def make(cls, make_default: Callable[[Config], T]) -> Any:
-        """Creates a default sentinel that is resolved after the config is validated.
-
-        Args:
-            make_default: Callable that takes the validated config and returns the
-                resolved value.
-        """
-        return Field(default=cls(make_default), validate_default=False)
-
 
 class Config(BaseSettings):
     """Ragna configuration"""
+
+    __config_path__: ClassVar[Path | None] = None
 
     model_config = SettingsConfigDict(env_prefix="ragna_")
 
@@ -70,59 +39,47 @@ class Config(BaseSettings):
         dotenv_settings: PydanticBaseSettingsSource,
         file_secret_settings: PydanticBaseSettingsSource,
     ) -> tuple[PydanticBaseSettingsSource, ...]:
-        # This order is needed to prioritize values from environment variables over
-        # values from a configuration file. However, since the config instantiation from
-        # a config file goes through the regular constructor of the Python object, we
-        # are also implicitly prioritizing environment variables over values passed
-        # explicitly passed to the constructor. For example, if the environment variable
-        # 'RAGNA_LOCAL_ROOT' is set, any values passed to `Config(local_root=...)` are
-        # ignored.
-        # FIXME: Find a way to achieve the following priorities:
-        #  1. Explicitly passed to Python object
-        #  2. Environment variable
-        #  3. Configuration file
-        #  4. Default
-        return env_settings, init_settings
+        sources = [init_settings, env_settings]
+        if cls.__config_path__ is not None:
+            sources.append(
+                TomlConfigSettingsSource(settings_cls, toml_file=cls.__config_path__)
+            )
+        return tuple(sources)
 
     local_root: Annotated[Path, AfterValidator(make_directory)] = Field(
         default_factory=ragna.local_root
     )
 
-    auth: ImportString[type[Auth]] = "ragna.deploy.NoAuth"  # type: ignore[assignment]
-    key_value_store: ImportString[type[KeyValueStore]] = (
-        "ragna.deploy.InMemoryKeyValueStore"  # type: ignore[assignment]
-    )
+    auth: ImportString[type[Auth]] = Field(
+        default="ragna.deploy.NoAuth", validate_default=True
+    )  # type: ignore[assignment]
+    key_value_store: ImportString[type[KeyValueStore]] = Field(
+        default="ragna.deploy.InMemoryKeyValueStore", validate_default=True
+    )  # type: ignore[assignment]
 
-    document: ImportString[type[Document]] = "ragna.core.LocalDocument"  # type: ignore[assignment]
-    source_storages: list[ImportString[type[SourceStorage]]] = [
-        "ragna.source_storages.RagnaDemoSourceStorage"  # type: ignore[list-item]
-    ]
-    assistants: list[ImportString[type[Assistant]]] = [
-        "ragna.assistants.RagnaDemoAssistant"  # type: ignore[list-item]
-    ]
+    document: ImportString[type[Document]] = Field(
+        default="ragna.core.LocalDocument", validate_default=True
+    )  # type: ignore[assignment]
+    source_storages: list[ImportString[type[SourceStorage]]] = Field(
+        default_factory=lambda: ["ragna.source_storages.RagnaDemoSourceStorage"],  # type: ignore[arg-type]
+        validate_default=True,
+    )
+    assistants: list[ImportString[type[Assistant]]] = Field(
+        default_factory=lambda: ["ragna.assistants.RagnaDemoAssistant"],  # type: ignore[arg-type]
+        validate_default=True,
+    )
 
     hostname: str = "127.0.0.1"
     port: int = 31476
     root_path: str = ""
-    origins: list[str] = AfterConfigValidateDefault.make(
-        lambda config: [f"http://{config.hostname}:{config.port}"]
+    origins: list[str] = Field(
+        default_factory=lambda values: [f"http://{values['hostname']}:{values['port']}"]
     )
     session_lifetime: int = 60 * 60 * 24
 
-    database_url: str = AfterConfigValidateDefault.make(
-        lambda config: f"sqlite:///{config.local_root}/ragna.db",
+    database_url: str = Field(
+        default_factory=lambda values: f"sqlite:///{values['local_root']}/ragna.db"
     )
-
-    @model_validator(mode="after")
-    def _validate_model(self) -> Config:
-        self._resolve_default_sentinels(self)
-        return self
-
-    def _resolve_default_sentinels(self, config: Config) -> None:
-        for name, info in type(self).model_fields.items():
-            value = getattr(self, name)
-            if isinstance(value, AfterConfigValidateDefault):
-                setattr(self, name, value.make_default(config))
 
     @property
     def _url(self) -> str:
@@ -152,8 +109,9 @@ class Config(BaseSettings):
         if not path.is_file():
             raise RagnaException(f"{path} does not exist.")
 
-        with open(path) as file:
-            return cls.model_validate(tomlkit.load(file).unwrap())
+        return cast(
+            type[Config], type(cls.__name__, (cls,), {"__config_path__": path})
+        )()
 
     def to_file(self, path: Union[str, Path], *, force: bool = False) -> None:
         path = Path(path).expanduser().resolve()
